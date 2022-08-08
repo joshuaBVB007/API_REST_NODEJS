b9c0da56213b0ebb24d257a8a6d7e73bd2250f6e
Jonathan Calderon <jaksvafa7@gmail.com>
2022-08-08 19:12:56 +0200

    refactoring: less lines and doing the same thing

--- src/db_connection.js
+++ src/db_connection.js
@@ -1,36 +1,15 @@
-//creamos una variable con la conexion a mysql
-var mysql = require('mysql');
+import mysql from "mysql"
 
-var con = mysql.createConnection({
+export var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "root",
-  database:"yasai"
+  password: "password",
+  database:"world"
 });
 
-con.connect(function(err) {
+connection.connect(function(err) {
   if (err){
-    throw err;
-    console.log(err);
+    console.log("Ha fallado la conexion:");
   } 
   console.log("Connected!");   
 });
-
-module.exports=con;
-
-//Refactoring
-
-
-/*
-si te resulta dar este error
-Error: ER_NOT_SUPPORTED_AUTH_MODE: Client does not support authentication protocol requested by server; consider upgrading MySQL client
-
-ve a workbench y ejecutas el siguiente codigo
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
-
-luego ve a tu carpeta donde esté este fichero y ejecutalo
-ej: cd bd_file
-    node nomfichero.js
-    si te sale connected o el feedback que le hayas puesto esta todo perfecto puedes continuar
-*/
-
--- src/server.js
+++ src/server.js
@@ -1,41 +1,40 @@
-var express = require('express');
+import express from "express";
+import { connection } from "../src/db_connection.js"
 var app = express();
-var con= require('./db_connection');
-
-//app.use(express.json()) // for parsing application/json
-
-const bodyParser = require('body-parser')
-
-app.use(bodyParser.json()) // for parsing application/json
 
 app.get("/", function (req, res){
     console.log("SELECT");
-    con.query("SELECT * FROM frutas", function (err, result, fields) {
-        if (!err){ res.json(result); console.log("Query succesfully");} 
-        else{ console.log(err);}
+    connection.query("SELECT * FROM COUNTRY", function (err, result, fields) {
+        if (!err){ 
+          res.json(result); console.log("Query succesfully");
+        } 
+        else{ 
+          console.log(err);
+        }
     });
 });
 
-app.get("/:id",function(req,res){
-  console.log("SELECT con ID");
-  const { id }=req.params;
-  var sql = "select * from frutas WHERE ID_fruit = ?";
-  con.query(sql,[id],(err, result) => {
-    if (!err){
-        res.json(result[0]);
-    }
-    console.log(result.affectedRows + " record(s) updated");
-  });
-});
+// app.get("/:id",function(req,res){
+//   console.log("SELECT con ID");
+//   const { id }=req.params;
+//   var sql = "select * from COUNTRY WHERE NAME = ?";
+//   con.query(sql,[id],(err, result) => {
+//     if (!err){
+//         res.json(result[0]);
+//     }else{
+//       console.log(err)
+//     }
+//   });
+// });
 
-app.post("/",function(req,res){
-  const { id,nombre }=req.body;
-  var sql = "insert into frutas values (?,?)";
-  con.query(sql,[id,nombre],(err, result) => {
-    if (err) throw err;
-    console.log("1 record inserted");
-  });
-});
-
+// app.post("/",function(req,res){
+//   const { id,nombre }=req.body;
+//   var sql = "insert into frutas values (?,?)";
+//   con.query(sql,[id,nombre],(err, result) => {
+//     if (err) throw err;
+//     console.log("1 record inserted");
+//   });
+// });
 
 app.listen(9000);
+console.log("Escribe en tu navegador el localhost:9000")
